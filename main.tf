f596698f0e713dd7a28e54d7642ac3e046ece24c
raviteja <ravitejatfcdemo@gmail.com>
2023-02-21 16:01:24 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -22,15 +22,6 @@ resource "aws_security_group" "rabbitmq" {
 }
 
 
-#resource "aws_mq_configuration" "rabbitmq" {
-#  description    = "${var.env}-rabbitmq-configuration"
-#  name           = "${var.env}-rabbitmq-configuration"
-#  engine_type    = var.engine_type
-#  engine_version = var.engine_version
-#  data = ""
-#}
-
-
 resource "aws_mq_broker" "rabbitmq" {
   broker_name        = "${var.env}-rabbitmq
   deployment_mode    = var.deployment_mode
@@ -40,11 +31,6 @@ resource "aws_mq_broker" "rabbitmq" {
   security_groups    = [aws_security_group.rabbitmq.id]
   subnet_ids         = var.deployment_mode == "SINGLE_INSTANCE" ? [var.subnet_ids[0]] : var.subnet_ids
 # if deployment mode is single instance then [var.subnet_ids[0] is the value, ifnot take all subnet id's
-
-#  configuration {
-#    id       = aws_mq_configuration.rabbitmq.id
-#    revision = aws_mq_configuration.rabbitmq.latest_revision
-#  }
 
   encryption_options {
     use_aws_owned_key = false
