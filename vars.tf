b1346bb6341df544b708b6fa174ffea4eabe71c8
raviteja <ravitejatfcdemo@gmail.com>
2023-03-07 18:32:11 +0530

    Terraform

--- vars.tf
+++ vars.tf
@@ -2,10 +2,10 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
-variable "engine_type" {}
-variable "engine_version" {}
-variable "host_instance_type" {}
-variable "deployment_mode" {}
+#variable "engine_type" {}
+#variable "engine_version" {}
+#variable "host_instance_type" {}
+#variable "deployment_mode" {}
 variable "bastion_cidr" {}
 variable "component" {
   default = "rabbitmq"
